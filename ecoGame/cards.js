d6a0c61767f78b77785e5111da310d94ac6c2e03
ashu-rb <78752052+ashu-rb@users.noreply.github.com>
2023-01-01 18:57:46 +0530

    Added to files

---
+++ ecoGame/cards.js
@@ -0,0 +1,147 @@
+
+
+var scenarios = [
+  {
+    hackerCard : {
+      description : "Would you buy an electric car?",
+      power : 4,
+    },
+    playerCards : [
+      {
+        description : "I drive one now and I love it",
+        power : 5,
+      },
+      {
+        description : "Maybe, if I had the money or if they were big enough to hold my family.",
+        power : 3,
+      },
+      {
+        description : "No I love the smell of gasoline.",
+        power : 1,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "Do you follow any 5R's(Reduce, Reuse, Repair, Rot, Recycle) of Environmnent?",
+      power : 3,
+    },
+    playerCards : [
+      {
+        description : "I always do,and also encourage others.",
+        power : 5,
+      },
+      {
+        description : "Only 3R's recycle,reuse,reduce some stuff when I remember.",
+        power : 3,
+      },
+      {
+        description : "Never.",
+        power : 1,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "What do you do with used batteries?",
+      power : 3,
+    },
+    playerCards : [
+      {
+        description : "I use rechargeable batteries.",
+        power : 5,
+      },
+      {
+        description : "Replace them with rechargeable batteries",
+        power : 4,
+      },
+      {
+        description : "I throw them away.",
+        power : 1,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : " Do you flush your un-used prescription drugs down the toilet?",
+      power : 2,
+    },
+    playerCards : [
+      {
+        description : "Never,I give them back to the pharmacy.",
+        power : 4,
+      },
+      {
+        description : "No, they just hang out in the cabinet.",
+        power : 2,
+      },
+      {
+        description : "Yes,very often I do that.",
+        power : 1,
+      }
+    ]
+  },
+  {
+    hackerCard : {
+      description : "What do you use to carry your groceries?.",
+      power : 2,
+    },
+    playerCards : [
+      {
+        description : "Re-usable canvas bags",
+        power : 4,
+      },
+      {
+        description : "Paper or plastic bags that I recycle after",
+        power : 3,
+      },
+      {
+        description : "Always plastic bag, use and throw them.",
+        power : 1,
+      }
+    ]
+  }, 
+
+  {
+    hackerCard : {
+      description : "How often do you use eco-friendly/public means of transport?",
+      power : 2,
+    },
+    playerCards : [
+      {
+        description : "Most of the time, I prefer public transport.",
+        power : 4,
+      },
+      {
+        description : "Sometimes, mainly I go with Carpooling services.",
+        power : 3,
+      },
+      {
+        description : "Always use my personal vehicle.",
+        power : 1,
+      }
+    ]
+  }, 
+
+  
+  {
+    hackerCard : {
+      description : "What about your Garden",
+      power : 2,
+    },
+    playerCards : [
+      {
+        description : "Large in size, providing food for more people than just my own family",
+        power : 4,
+      },
+      {
+        description : "Small or a container garden providing some herbs, lettuce, etc. for my own use",
+        power : 3,
+      },
+      {
+        description : "I don't have a garden",
+        power : 1,
+      }
+    ]
+  }
+];
